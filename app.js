86c84d3eed175ae6c15670627d351dc9bdec6dd1
hadimktri <116309036+hadimktri@users.noreply.github.com>
2023-02-02 16:51:41 -0800

    EJS

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express');                                             // npm i express installed
+const bodyParser = require('body-parser');                                       // npm i body-parser installed
+const path = require('path');                                                   // npm i path installed
 
- app.set('views', 'views'); 
+const app = express();                                                          // app uses express 
+app.use(bodyParser.urlencoded({ extended: false }));                           // default method to use the body parser
 
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.set('viw engine', 'ejs');                                                    // Views config for ejs
+app.set('views', 'views');                                                      // Views routing
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'views','index.html'));
-})
+const adminRoute = require('./routes/admin');                                    //name specified to the routes
+const shopRoute = require('./routes/shop');
+
+app.use(express.static(path.join(__dirname, 'public')));                        // Static files routing
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'index.html'));               // sendFile for sendig file through the html---------> example 
+// }) 
+
+app.use('/admin', adminRoute);                                                   // admin needed here because it's not the primary root of app
+app.use(shopRoute);
 
 app.listen(3000, () => {
-    console.log('Runnig on Port 3000...');
+    console.log('Runnig on Port 3000...')
 });
+
---
+++ routes/admin.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const router = express.Router() // to use the router feature of express
+
+router.get('/add-product', (req, res) => {
+
+})
+
+module.exports= router;
