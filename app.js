3eee9f3d358aa914d02c92b33a94bc34d3f54759
hadimktri <116309036+hadimktri@users.noreply.github.com>
2023-02-07 15:17:00 -0800

    73.1

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const User = require('./models/user');
-app.use(bodyParser.urlencoded({ extended: false }));
-
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-
+const User = require('./models/user');
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-app.use(express.static(path.join(__dirname, 'public')));
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
-const { findById } = require('./models/product');
 
-app.use((req, res, next) => { findById("63df754bf23d8921f2b68db5").then(user => { req.user = user; next(); })
-.catch(err => console.log(err)) })
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById("63df754bf23d8921f2b68db5").then(user => { req.user = user; next(); })
+        .catch(err => console.log(err))
+})
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
